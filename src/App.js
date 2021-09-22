90d2d94fee29f4c57c092c39fbc22f0b1aba4921
anahi <an211291@hotmail.com>
2021-09-21 20:11:28 -0500

    navbar delete

--- src/App.js
+++ src/App.js
@@ -17,10 +17,15 @@ import { Velasquez } from "./components/es/autores/velasquez";
 import { Vila } from "./components/es/autores/vila";
 import { Orozco } from "./components/es/autores/orozco";
 import { Iconografia } from "./components/es/iconografia/Iconografia";
-import { HomeEn } from "./components/en/homen/HomeEn";
-import {EnglishRouter} from "./EnglishRouter"
 
+import { HomeEn } from "./components/en/homen/HomeEn";
 import { Introduction } from "./components/en/introduction/Introduction";
+import { Codex } from "./components/en/codex/Codex";
+import { Iconography } from "./components/en/iconografia/Iconografia";
+import { Studies } from "./components/en/studies/Studies";
+import { EditorialProject } from "./components/en/editorial-project/EditorialProject";
+import { Downloads } from "./components/en/downloads/Downloads";
+import { Credits } from "./components/en/credits/Credits";
 
 
 import audio1 from "./audio/lamina_1.mp3";
@@ -138,10 +143,22 @@ function App() {
 
           <Route path="/en/" component={HomeEn}></Route>
 
-          <Route path="/en/" component={EnglishRouter}></Route>
+          <Route path="/en/introduction" exact component={Introduction}></Route>
 
+          <Route path="/en/codex" component={Codex}></Route>
 
-          <Route path="/en/introduction" component={Introduction}></Route>
+          <Route path="/en/iconography" component={Iconography}></Route>
+
+          <Route path="/en/studies" component={Studies}></Route>
+
+          <Route
+            path="/en/editorial-project"
+            component={EditorialProject}
+          ></Route>
+
+          <Route path="/en/downloads" component={Downloads}></Route>
+
+          <Route path="/en/credits" component={Credits}></Route>
  
 
           
